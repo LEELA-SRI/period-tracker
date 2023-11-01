299c7433b35cea9e8b5e23844c5f24ef2b6a3586
Leela Sri <leelasric@gmail.com>
2023-11-02 03:47:18 +0530

    last try

--- period-tracker/backend/app.py
+++ period-tracker/backend/app.py
@@ -34,8 +34,9 @@ def get_date():
         return jsonify({'dates': dates})
     else:
         return jsonify({'dates': []})
+    
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(port=5000, debug=True)
